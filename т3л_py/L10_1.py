774d773403edb57ae9ec72283c1c170eae156fff
YakovBobovskiy <bobovskiiyakov@gmail.com>
2025-10-04 16:41:21 +0500

    Delete т3л_py directory

--- т3л_py/L10_1.py
+++
@@ -1,9 +0,0 @@
-array = [2, 4, 6, 8, 9]
-flag = False
-for value in array:
-    if value % 2 == 1:
-        flag = True
-if flag:
-    print('В массиве есть нечетное число')
-else:
-    print('В массиве все числа четные')
